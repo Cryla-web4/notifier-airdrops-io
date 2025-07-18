f927170474945536764baaae62fc4f5db7dab523
Cryla-web4 <sakozo.com@gmail.com>
2025-07-18 12:37:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+
   await page.goto('https://airdrops.io/', { waitUntil: 'networkidle2' });
 
-  // しっかり描画を待つ
-  await page.waitForSelector('.container .list');
+  // 描画が終わるまでしっかり待つ
+  await page.waitForSelector('.list.list-airdrops .list-item');
 
-  // スクレイピング
+  // スクレイピング処理
   const data = await page.evaluate(() => {
-    const cards = document.querySelectorAll('.container .list .list-item');
+    const cards = document.querySelectorAll('.list.list-airdrops .list-item');
 
     return Array.from(cards).map(card => {
-      const title = card.querySelector('h5')?.innerText ?? 'No Title';
-      const description = card.querySelector('p')?.innerText ?? 'No Description';
+      const title = card.querySelector('h5')?.innerText.trim() ?? 'No Title';
+      const description = card.querySelector('p')?.innerText.trim() ?? 'No Description';
       const link = card.querySelector('a')?.href ?? 'No Link';
       return { title, description, link };
     });
